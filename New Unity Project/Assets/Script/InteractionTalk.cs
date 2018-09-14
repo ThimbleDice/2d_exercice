3c11b8cb31b3e28b90d188cccf7c634a97a5f4df
B533P-19\533 <thimbledice>
2018-09-14 14:50:21 -0400

    Cours 7

--- New Unity Project/Assets/Script/InteractionTalk.cs
+++ New Unity Project/Assets/Script/InteractionTalk.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class InteractionTalk : Interaction {
 
+    DialogManager dialogManager;
+    [SerializeField] DialogText dialogText;
+
     public override void Interact()
     {
-        print("Hello");
+        dialogManager.StartDialog(dialogText);
     }
 
     // Use this for initialization
     void Start () {
-		
+        dialogManager = GameObject.FindGameObjectWithTag("DialogManager").GetComponent<DialogManager>();
 	}
 	
 	// Update is called once per frame
